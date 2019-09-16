bd8f240eacc92fc28bb5e7069b8c528311427d25
Prokhorov Alexander <streamgroupru@gmail.com>
2019-09-17 01:06:51 +0300

    1

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>ОН ЁБАНЫЙ ВОЛШЕБНИК, ГАРИ</Text>
+      <Text>ОН ЁБАНЫЙ ВОЛШЕБНИК,  ГАРИ</Text>
     </View>
   );
 }
